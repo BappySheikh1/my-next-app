bb242dd43806c6a202c7ea71c1a77b8f2276d05a
BappySheikh1 <sheikhbappy91gmail.com>
2023-07-22 01:33:10 +0600

    ant design added

---
+++ src/pages/404.js
@@ -0,0 +1,14 @@
+
+const NotFountPage = () => {
+  return (
+    <div>
+      <img
+        src="https://w7.pngwing.com/pngs/336/743/png-transparent-error-404-404-error.png"
+        alt=""
+        width="100%"
+      />
+    </div>
+  );
+};
+
+export default NotFountPage;
