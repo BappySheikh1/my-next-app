c46d599b79d4be583f8ba76712700bb9ff740b5c
BappySheikh1 <sheikhbappy91gmail.com>
2023-07-22 01:14:55 +0600

    first commit

---
+++ src/pages/article/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Article = () => {
+    return (
+        <div>
+            <h1>Welcome to article home page</h1>
+        </div>
+    );
+};
+
+export default Article;
